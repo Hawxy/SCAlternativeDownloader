dae07000cf4ee8d7a5886930031ec540883acc4e
Hawx <Hawxy@users.noreply.github.com>
2017-04-05 12:30:18 +0800

    Assembly version

--- SCPatchDownloader/Properties/AssemblyInfo.cs
+++ SCPatchDownloader/Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("SCPatchDownloader")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Zephyr")]
 [assembly: AssemblyProduct("SCPatchDownloader")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyCopyright("Copyright Hawx ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -33,4 +33,4 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("2.2.3.0")]
+[assembly: AssemblyFileVersion("2.2.4.0")]
