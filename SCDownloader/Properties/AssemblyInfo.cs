526d389424223e06e1133d15236f417ec81e518c
Hawx <Hawxy@users.noreply.github.com>
2017-09-29 21:58:53 +0800

    Update assembly

--- SCDownloader/Properties/AssemblyInfo.cs
+++ SCDownloader/Properties/AssemblyInfo.cs
@@ -7,12 +7,12 @@ using System.Windows;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("materialtestWPF")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("SCDownloader")]
+[assembly: AssemblyDescription("Star Citizen file downloader")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("materialtestWPF")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyProduct("SCDownloader")]
+[assembly: AssemblyCopyright("Copyright Hawx & Zephyr Aux ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -51,5 +51,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("4.0.0.0")]
+[assembly: AssemblyFileVersion("4.0.0.0")]
